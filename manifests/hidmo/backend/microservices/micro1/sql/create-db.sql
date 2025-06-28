fa67d4fe81a109156e04b1cfde0b428986f303dd
leultewolde <leulwoldegabriel@gmail.com>
2025-06-27 23:15:18 -0400

    Simplify database creation by removing conditional PL/pgSQL block for micro1

--- manifests/hidmo/backend/microservices/micro1/sql/create-db.sql
+++ manifests/hidmo/backend/microservices/micro1/sql/create-db.sql
@@ -1,10 +1,1 @@
-DO
-    $$
-    BEGIN
-      IF NOT EXISTS (
-        SELECT FROM pg_database WHERE datname = 'micro1'
-      ) THEN
-        CREATE DATABASE micro1;
-      END IF;
-    END
-    $$;
+CREATE DATABASE micro1;
